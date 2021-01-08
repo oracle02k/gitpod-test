d663ffc7dc7d124bf61a8c39e3b37c1609bbf5dd
oracle02k <oracle02k@gmail.com>
2021-01-08 13:29:28 +0000

    formを追加

--- public/main.php
+++ public/main.php
@@ -2,6 +2,10 @@
 
 <html>
 <body>
-<?php echo 'php エコー'?>
+<?= 'php エコー'?>
+<form method="POST" action="api.php">
+    <p>お名前<input type=TEXT name="NAME"></p>
+    <input type="submit" value="送信する">
+</form>
 </body>
 </html>
